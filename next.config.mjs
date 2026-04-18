62bc452f55d981429b93193c84199ec32f56045f
Stevens monareng <stevens.monareng@gmail.com>
2026-04-18 15:40:32 +0200

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -5,13 +5,16 @@ const nextConfig = {
           {
             protocol: 'https',
             hostname: 'links.papareact.com',
+            pathname: '**',
           },
           {
             protocol: 'https',
             hostname: 'i5.walmartimages.com',
+            pathname: '**',
           }
         ],
       },
+    output: 'standalone'  
 };
 
 export default nextConfig;
